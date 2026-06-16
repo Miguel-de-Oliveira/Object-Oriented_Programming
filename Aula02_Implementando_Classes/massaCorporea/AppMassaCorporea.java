9fdd483df449a08a3b27d482369a753b94704435
Miguel Luiz <miguelluizldeoliveira@gmail.com>
2026-06-15 22:05:47 -0300

    Aplicação Massa Corpórea

---
+++ Aula02_Implementando_Classes/massaCorporea/AppMassaCorporea.java
@@ -0,0 +1,22 @@
+package massaCorporea;
+import java.util.Scanner;
+
+public class AppMassaCorporea {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		MassaCorporea mc = new MassaCorporea();
+		
+		System.out.println("Peso..: ");
+		mc.peso = input.nextDouble();
+		System.out.println("Altura: ");
+		mc.altura = input.nextDouble();
+		//Chamar os métodos para usar os valores inseridos
+		mc.calcularImc();
+		mc.definirSituacao();
+		
+		System.out.printf("IMC.....: %.1f\n", mc.imc);
+		System.out.printf("Situação: %s", mc.situacao);
+		input.close();
+	}
+
+}
