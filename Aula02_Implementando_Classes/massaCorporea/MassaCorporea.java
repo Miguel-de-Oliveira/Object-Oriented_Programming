9c5e6b976f4cafe023b0cd53d395ff05e7d41789
Miguel Luiz <miguelluizldeoliveira@gmail.com>
2026-06-15 22:06:20 -0300

    Correção na estrutura de decisão

--- Aula02_Implementando_Classes/massaCorporea/MassaCorporea.java
+++ Aula02_Implementando_Classes/massaCorporea/MassaCorporea.java
@@ -13,9 +13,11 @@ public class MassaCorporea {
 	
 	void definirSituacao() {
 		if(imc < 17) {situacao = "Muito abaixo do peso.";}
-		if(imc < 18.5) {situacao = "Abaixo do peso.";}
-		if(imc < 25) {situacao = "Peso normal.";}
-		if(imc < 30) {situacao = "Acima do peso.";}
-		if(imc < 35) {situacao = "Obesidade grau 1";}
+		else if(imc < 18.5) {situacao = "Abaixo do peso.";}
+		else if(imc < 25) {situacao = "Peso normal.";}
+		else if(imc < 30) {situacao = "Acima do peso.";}
+		else if(imc < 35) {situacao = "Obesidade grau 1";}
+		else if(imc < 40) {situacao = "Obesidade grau 2";}
+		else {situacao = "Obesidade grau 3";}
 	}
 }
