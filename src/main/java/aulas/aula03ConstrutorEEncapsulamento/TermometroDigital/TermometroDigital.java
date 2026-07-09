ea8733bcda42ba26fcf08695589091e9e3de8671
Miguel Luiz <miguelluizldeoliveira@gmail.com>
2026-07-09 20:53:43 -0300

    Classe TermometroDigital.

---
+++ src/main/java/aulas/aula03ConstrutorEEncapsulamento/TermometroDigital/TermometroDigital.java
@@ -0,0 +1,51 @@
+package aulas.aula03ConstrutorEEncapsulamento.TermometroDigital;
+
+public class TermometroDigital {
+	private double temperatura;
+	private String nrSerie;
+	private char tipo;
+	private String diagnostico;
+	
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+
+	public TermometroDigital(String nrSerie, char tipo) {
+		this.nrSerie = nrSerie;
+		this.tipo = tipo;
+	}
+
+	public double getTemperatura() {
+		return temperatura;
+	}
+
+	public char getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(char tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setTemperatura(double temperatura) {
+		this.temperatura = temperatura;
+	}
+
+	public String getNrSerie() {
+		return nrSerie;
+	}
+	
+	public void aferirTemperatura(double temperatura) {
+		this.temperatura = temperatura;
+		lerDiagnostico();
+	}
+	
+	private String lerDiagnostico() {
+		if(this.temperatura < 35.5) {return this.diagnostico = "Hipotermia";}
+		if(this.temperatura < 37)   {return this.diagnostico = "Normal";}
+		if(this.temperatura < 37.9) {return this.diagnostico = "Estado Febril";}
+		if(this.temperatura < 38.9) {return this.diagnostico = "Febre";}
+		if(this.temperatura < 39.9) {return this.diagnostico = "Febre Alta";}
+		return this.diagnostico = "Hipertermia";
+	}
+}
